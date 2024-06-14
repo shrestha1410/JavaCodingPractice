137dff937e7552cd5200e321a16d67abc6f3f67d
Shrestha <shresthasah1410@gmail.com>
2024-06-14 10:26:53 +0530

    added question

---
+++ src/main/java/org/example/Question1.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Write a program to check if the letter 'e' is present in the word 'Umbrella'. using stream
+ */
+
+public class Question1 {
+ public static void main(String[] args){
+    String s="Umbrella";
+    System.out.println(s.chars().anyMatch(c ->c=='e' ));
+
+ }
+    
+}
